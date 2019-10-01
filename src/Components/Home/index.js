1d9cd651f9e7f00e58d89deca86db0cb7b483971
janakhpon <jnovaxer@gmail.com>
2019-10-01 12:42:55 +0630

    GDEV001

---
+++ src/Components/Home/index.js
@@ -0,0 +1,221 @@
+import React from 'react';
+import image_1 from '../images/image_1.jpg';
+import image_2 from '../images/image_2.jpg';
+import image_3 from '../images/image_3.jpg';
+import image_4 from '../images/image_4.jpg';
+import image_5 from '../images/image_5.jpg';
+import image_6 from '../images/image_6.jpg';
+import image_7 from '../images/image_7.jpg';
+import image_8 from '../images/image_8.jpg';
+import image_9 from '../images/image_9.jpg';
+import image_10 from '../images/image_10.jpg';
+import image_11 from '../images/image_11.jpg';
+import image_12 from '../images/image_12.jpg';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div id="colorlib-page">
+                    <a href="/" className="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
+                    <aside id="colorlib-aside" role="complementary" className="js-fullheight text-center">
+                        <h1 id="colorlib-logo"><a href="index.html"><span className="flaticon-camera"></span>Capture</a></h1>
+                        <nav id="colorlib-main-menu" role="navigation">
+                            <ul>
+                                <li className="colorlib-active"><a href="index.html">Home</a></li>
+                                <li><a href="gallery.html">Gallery</a></li>
+                                <li><a href="about.html">About</a></li>
+                                <li><a href="blog.html">Blog</a></li>
+                                <li><a href="contact.html">Contact</a></li>
+                            </ul>
+                        </nav>
+
+                        <div className="colorlib-footer">
+                            <h3>Follow Us Here!</h3>
+                            <div className="d-flex justify-content-center">
+                                <ul className="d-flex align-items-center">
+                                    <li className="d-flex align-items-center jusitfy-content-center"><a href="/"><i className="icon-facebook"></i></a></li>
+                                    <li className="d-flex align-items-center jusitfy-content-center"><a href="/"><i className="icon-twitter"></i></a></li>
+                                    <li className="d-flex align-items-center jusitfy-content-center"><a href="/"><i className="icon-instagram"></i></a></li>
+                                    <li className="d-flex align-items-center jusitfy-content-center"><a href="/"><i className="icon-linkedin"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </aside>
+                    <div id="colorlib-main">
+                        <section className="ftco-section-2">
+                            <div className="photograhy">
+                                <div className="row no-gutters">
+                                    <div className="col-md-4 ftco-animate">
+                                        <a href="images/image_1.jpg" className="photography-entry img image-popup d-flex justify-content-center align-items-center" style={{ backgroundImage: `url(${image_1})` }} >
+                                            <div className="overlay"></div>
+                                            <div className="text text-center">
+                                                <h3>Work 01</h3>
+                                                <span className="tag">Model</span>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div className="col-md-4 ftco-animate">
+                                        <a href="images/image_2.jpg" className="photography-entry img image-popup d-flex justify-content-center align-items-center" style={{ backgroundImage: `url(${image_1})` }}>
+                                            <div className="overlay"></div>
+                                            <div className="text text-center">
+                                                <h3>Work 02</h3>
+                                                <span className="tag">Nature</span>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div className="col-md-4 ftco-animate">
+                                        <a href="images/image_3.jpg" className="photography-entry img image-popup d-flex justify-content-center align-items-center" style={{ backgroundImage: `url(${image_1})` }}>
+                                            <div className="overlay"></div>
+                                            <div className="text text-center">
+                                                <h3>Work 03</h3>
+                                                <span className="tag">Fashion</span>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div className="col-md-4 ftco-animate">
+                                        <a href="images/image_4.jpg" className="photography-entry img image-popup d-flex justify-content-center align-items-center" style={{ backgroundImage: `url(${image_1})` }}>
+                                            <div className="overlay"></div>
+                                            <div className="text text-center">
+                                                <h3>Work 04</h3>
+                                                <span className="tag">Travel</span>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div className="col-md-4 ftco-animate">
+                                        <a href="images/image_5.jpg" className="photography-entry img image-popup d-flex justify-content-center align-items-center" style={{ backgroundImage: `url(${image_1})` }}>
+                                            <div className="overlay"></div>
+                                            <div className="text text-center">
+                                                <h3>Work 05</h3>
+                                                <span className="tag">Travel</span>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div className="col-md-4 ftco-animate">
+                                        <a href="images/image_6.jpg" className="photography-entry img image-popup d-flex justify-content-center align-items-center" style={{ backgroundImage: `url(${image_1})` }}>
+                                            <div className="overlay"></div>
+                                            <div className="text text-center">
+                                                <h3>Work 06</h3>
+                                                <span className="tag">Travel</span>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div className="col-md-4 ftco-animate">
+                                        <a href="images/image_7.jpg" className="photography-entry img image-popup d-flex justify-content-center align-items-center" style={{ backgroundImage: `url(${image_1})` }}>
+                                            <div className="overlay"></div>
+                                            <div className="text text-center">
+                                                <h3>Work 07</h3>
+                                                <span className="tag">Fashion, Model</span>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div className="col-md-4 ftco-animate">
+                                        <a href="images/image_8.jpg" className="photography-entry img image-popup d-flex justify-content-center align-items-center" style={{ backgroundImage: `url(${image_1})` }}>
+                                            <div className="overlay"></div>
+                                            <div className="text text-center">
+                                                <h3>Work 08</h3>
+                                                <span className="tag">Nature</span>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div className="col-md-4 ftco-animate">
+                                        <a href="images/image_9.jpg" className="photography-entry img image-popup d-flex justify-content-center align-items-center" style={{ backgroundImage: `url(${image_1})` }}>
+                                            <div className="overlay"></div>
+                                            <div className="text text-center">
+                                                <h3>Work 09</h3>
+                                                <span className="tag">Technology</span>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div className="col-md-4 ftco-animate">
+                                        <a href="images/image_10.jpg" className="photography-entry img image-popup d-flex justify-content-center align-items-center" style={{ backgroundImage: `url(${image_1})` }}>
+                                            <div className="overlay"></div>
+                                            <div className="text text-center">
+                                                <h3>Work 10</h3>
+                                                <span className="tag">Model</span>
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div className="col-md-4 ftco-animate">
+                                        <a href="images/image_11.jpg" className="photography-entry img image-popup d-flex justify-content-center align-items-center" style={{ backgroundImage: `url(${image_1})` }}>
+                                            <div className="overlay"></div>
+                                            <div className="text text-center">
+                                                <h3>Work 11</h3>
+                                                <span className="tag">Fashion</span>
+                                            </div>
+                                        </a>
+                                    </div>
+
+
+                                    <div className="col-md-4 ftco-animate">
+                                        <a href="images/image_11.jpg" className="photography-entry img image-popup d-flex justify-content-center align-items-center" style={{ backgroundImage: `url(${image_1})` }}>
+                                            <div className="overlay"></div>
+                                            <div className="text text-center">
+                                                <h3>Work 11</h3>
+                                                <span className="tag">Fashion</span>
+                                            </div>
+                                        </a>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </section>
+                        <footer className="ftco-footer ftco-bg-dark ftco-section">
+                            <div className="container px-md-5">
+                                <div className="row mb-5">
+                                    <div className="col-md">
+                                        <div className="ftco-footer-widget mb-4 ml-md-4">
+                                            <h2 className="ftco-heading-2">Category</h2>
+                                            <ul className="list-unstyled categories">
+                                                <li><a href="/">Photography <span>(6)</span></a></li>
+                                                <li><a href="/">Fashion <span>(8)</span></a></li>
+                                                <li><a href="/">Technology <span>(2)</span></a></li>
+                                                <li><a href="/">Travel <span>(2)</span></a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div className="col-md">
+                                        <div className="ftco-footer-widget mb-4">
+                                            <h2 className="ftco-heading-2">Archives</h2>
+                                            <ul className="list-unstyled categories">
+                                                <li><a href="/">November 2018 <span>(105)</span></a></li>
+                                                <li><a href="/">October 2018 <span>(212)</span></a></li>
+                                                <li><a href="/">September 2018 <span>(150)</span></a></li>
+                                                <li><a href="/">August 2018 <span>(100)</span></a></li>
+                                                <li><a href="/">July 2018 <span>(200)</span></a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div className="col-md">
+                                        <div className="ftco-footer-widget mb-4">
+                                            <h2 className="ftco-heading-2">Have a Questions?</h2>
+                                            <div className="block-23 mb-3">
+                                                <ul>
+                                                    <li><span className="icon icon-map-marker"></span><span className="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
+                                                    <li><a href="/"><span className="icon icon-phone"></span><span className="text">+2 392 3929 210</span></a></li>
+                                                    <li><a href="/"><span className="icon icon-envelope"></span><span className="text">info@yourdomain.com</span></a></li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-md-12">
+
+                                        <p>
+                                            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i className="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </footer>
+                    </div>
+                </div>
+                <div id="ftco-loader" className="show fullscreen"><svg className="circular" width="48px" height="48px"><circle className="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="/eeeeee" /><circle className="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="/F96D00" /></svg></div>
+
+            </React.Fragment >
+        );
+    }
+}
+
+export default Home;
