10e6e8dbebbb5f3aedb155f9223e526187348854
janakhpon <jnovaxer@gmail.com>
2019-10-18 17:22:02 +0630

    GDEV023

--- src/Components/Uploadform/index.js
+++ src/Components/Uploadform/index.js
@@ -1,7 +1,6 @@
 import React, { Fragment, Component } from 'react';
 import Select from 'react-select';
 import axios from 'axios';
-const API_URL = 'http://localhost:5000/galarieapi';
 const TYPE_API_URL = 'http://localhost:5000/typeapi';
 const TAG_API_URL = 'http://localhost:5000/tagapi';
 const GALARIE_API_URL = 'http://localhost:5000/galarieapi';
@@ -9,6 +8,17 @@ const GALARIE_API_URL = 'http://localhost:5000/galarieapi';
 const descriptionOptions = [];
 const tagOptions = [];
 const typeOptions = [];
+const deviceOptions = [
+    { value: 'iPhone11', label: 'iPhone11' },
+    { value: 'iPhone11s', label: 'iPhone11s' },
+    { value: 'iPhoneXs', label: 'iPhoneXs' },
+    { value: 'iPhoneX', label: 'iPhoneX' },
+    { value: 'iPhone7s', label: 'iPhone7s' },
+    { value: 'iPhone7', label: 'iPhone7' },
+    { value: 'iPhone7sPro', label: 'iPhone7sPro' },
+    { value: 'iPhone7Pro', label: 'iPhone7Pro' },
+    { value: 'iPhone6sPro', label: 'iPhone6sPro' }
+];
 const formData = new FormData();
 
 class Uploadform extends Component {
@@ -19,10 +29,15 @@ class Uploadform extends Component {
         this.state = {
             selectedTypes: null,
             selectedTags: [],
+            selectedDevices: [],
             wallpapers: [],
-            desOptions:[],
+            desOptions: [],
             tags: [],
             types: [],
+            title: '',
+            choice: 0,
+            rate: 0,
+            description: '',
             image: null
         }
 
@@ -38,12 +53,12 @@ class Uploadform extends Component {
                 wallpapers: data
             });
 
+
             this.state.wallpapers.map((wallpaper, key) => {
-                let temp = { value:wallpaper.description, label:wallpaper.description};
+                let temp = { value: wallpaper.description, label: wallpaper.description };
                 return descriptionOptions.push(temp);
             });
 
-            console.log(descriptionOptions);
         });
 
     }
@@ -77,14 +92,18 @@ class Uploadform extends Component {
     }
 
 
-    postGalarie = () => {
-        
+
+    handleTitleChange = (e) => {
+        this.setState({
+            title: e.target.value
+        });
+        formData.set('title', e.target.value);
+
     }
 
     handleTypeChange = selectedTypes => {
         this.setState({ selectedTypes, type: selectedTypes.value });
         formData.set('type', selectedTypes.value);
-        console.log(`Option selected:`, selectedTypes.value);
     };
 
     handleTagChange = selectedTags => {
@@ -93,27 +112,59 @@ class Uploadform extends Component {
             //use not to repeat arrary value
             let tagHost = [...new Set(selectedTags.map(a => a.value))];
             formData.set('keyword', tagHost);
-            console.log('TagHOst', tagHost);
         } catch (err) {
             console.log("null is not accepatble");
         }
 
     }
 
-    handleImage = (e) => {
+    handleDeviceChange = selectedDevices => {
+        this.setState({ selectedDevices });
+        try {
+            //use not to repeat arrary value
+            let tagHost = [...new Set(selectedDevices.map(a => a.value))];
+            formData.set('devices', tagHost);
+        } catch (err) {
+            console.log("null is not accepatble");
+        }
+
+    }
+
+
+    handleDescriptionChange = (e) => {
+        this.setState({
+            description: e.target.value
+        });
+        formData.set('description', e.target.value);
+
+
+    }
+
+    handleRateChange = (e) => {
+        this.setState({ rate: e.target.value });
+        formData.set('rate', e.target.value);
+        console.log(e.target.value);
+    }
+    handleChoiceChange = (e) => {
+        this.setState({ choice: e.target.value });
+        formData.set('choice', e.target.value);
+
+    }
+
+    handleImageChange = (e) => {
         this.setState({
             image: e.target.files
         });
-
         formData.append('image', e.target.files[0])
-        console.log(e.target.files[0]);
+    }
+
+    postGalarie = () => {
+
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
-
-
-        const url = `${API_URL}/upload`;
+        const url = `${GALARIE_API_URL}/add`;
 
 
         axios({
@@ -145,10 +196,10 @@ class Uploadform extends Component {
     }
 
     render() {
-        const { selectedTypes, selectedTags } = this.state;
+        const { selectedTypes, selectedTags, selectedDevices } = this.state;
         const tagclasses = ['badge badge-pill badge-primary', 'badge badge-pill badge-secondary', 'badge badge-pill badge-success', 'badge badge-pill badge-danger', 'badge badge-pill badge-warning', 'badge badge-pill badge-info', 'badge badge-pill badge-light', 'badge badge-pill badge-dark'];
         const typeclasses = ['badge badge-primary', 'badge badge-secondary', 'badge badge-success', 'badge badge-danger', 'badge badge-warning', 'badge badge-info', 'badge badge-light', 'badge badge-dark'];
-        const descriptions = this.descriptionOptions;
+
         return (
             <Fragment>
                 <div className="site-section" data-aos="fade">
@@ -156,13 +207,6 @@ class Uploadform extends Component {
 
                         <div className="row justify-content-center">
                             <div className="col-md-7">
-                                <div className="row mb-5">
-                                    <div className="col-12 ">
-                                        <h2 className="site-section-heading text-center">Participate in Gallery</h2>
-
-                                    </div>
-                                </div>
-
                                 <div className="row">
                                     <div className="col-lg-8 mb-5">
                                         <form action="/">
@@ -182,7 +226,24 @@ class Uploadform extends Component {
 
                                                 <div className="col-md-12">
                                                     <label className="text-black" htmlFor="title">title</label>
-                                                    <input type="text" id="title" className="form-control" />
+                                                    <input type="text" id="title" className="form-control" onChange={this.handleTitleChange} />
+                                                </div>
+                                            </div>
+
+                                            <div className="row form-group">
+
+                                                <div className="col-md-12">
+                                                    <label className="text-black" htmlFor="tags">tags</label>
+                                                    <Select
+                                                        defaultValue={[deviceOptions[2], deviceOptions[3]]}
+                                                        isMulti
+                                                        name="colors"
+                                                        value={selectedDevices}
+                                                        onChange={this.handleDeviceChange}
+                                                        options={deviceOptions}
+                                                        className="basic-multi-select"
+                                                        classNamePrefix="select"
+                                                    />
                                                 </div>
                                             </div>
 
@@ -204,16 +265,30 @@ class Uploadform extends Component {
                                             </div>
 
                                             <div className="row form-group">
+
+                                                <div className="col-md-12">
+                                                    <label className="text-black" htmlFor="rate">Rate it out of 5</label>
+                                                    <input type="number" value={this.state.rate} id="rate" className="form-control" min="0" max="5" onChange={this.handleRateChange} />
+                                                </div>
+                                            </div>
+                                            <div className="row form-group">
+
+                                                <div className="col-md-12">
+                                                    <label className="text-black" htmlFor="choice">Choice out of 100</label>
+                                                    <input type="range" name="choice" id="choice" min="0" max="100" className="form-control" onChange={this.handleChoiceChange} />
+                                                </div>
+                                            </div>
+                                            <div className="row form-group">
                                                 <div className="col-md-12">
                                                     <label className="text-black" htmlFor="description">description</label>
-                                                    <textarea name="description" id="description" cols="30" rows="7" className="form-control" placeholder="Write your notes or questions here..."></textarea>
+                                                    <textarea name="description" id="description" cols="30" rows="7" className="form-control" onChange={this.handleDescriptionChange} placeholder="Write your notes or questions here..."></textarea>
                                                 </div>
                                             </div>
 
                                             <div className="row form-group">
                                                 <div className="col-md-12">
                                                     <label htmlFor="image">Your Art</label>
-                                                    <input type="file" className="form-control-file" id="image" name="image" ref={this.image} onChange={this.handleImage} />
+                                                    <input type="file" className="form-control-file" id="image" name="image" ref={this.image} onChange={this.handleImageChange} />
                                                 </div>
                                             </div>
 
@@ -230,7 +305,17 @@ class Uploadform extends Component {
                                     <div className="col-lg-3 ml-auto">
                                         <div className="mb-3 bg-white">
                                             <p className="mb-0 font-weight-bold">Desclaimer/Notes</p>
-                                            <p className="mb-4"></p>
+
+                                            {/*{
+                                                    descriptionOptions.map((des, key)=>{
+                                                        return (<p className="mb-4" key={key}>{des.value}</p>);
+                                                    })
+                                                } */}
+                                            {
+
+                                                (descriptionOptions.length === 0) ? <p className="mb-4">none</p> : <p className="mb-4">{descriptionOptions[Math.floor(Math.random() * descriptionOptions.length) + 0].value}</p>
+                                            }
+
 
                                             <p className="mb-0 font-weight-bold"><b>Types</b></p>
                                             <div className="mb-4">
@@ -238,14 +323,14 @@ class Uploadform extends Component {
                                                 starting from index 0 to 7
                                                 */}
                                                 {
-                                                    typeOptions.map((type, key)=>{
-                                                        let gid = Math.floor(Math.random()*7) +0;
+                                                    typeOptions.map((type, key) => {
+                                                        let gid = Math.floor(Math.random() * 7) + 0;
                                                         return (
-                                                                <span className={`${typeclasses[gid]}`} key={key}>{type.value}</span>
+                                                            <span className={`${typeclasses[gid]}`} key={key}>{type.value}</span>
                                                         );
                                                     })
                                                 }
-                                                
+
 
                                             </div>
 
@@ -255,13 +340,13 @@ class Uploadform extends Component {
 
                                                 {
                                                     tagOptions.map((tag, key) => {
-                                                        let gid = Math.floor(Math.random()*7) +0;
-                                                        return(
+                                                        let gid = Math.floor(Math.random() * 7) + 0;
+                                                        return (
                                                             <span className={`${tagclasses[gid]}`} key={key}>{tag.value}</span>
                                                         );
                                                     })
                                                 }
-                                            
+
                                             </div>
 
                                         </div>
