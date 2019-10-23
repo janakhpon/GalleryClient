a7519b6f908bfab781910e3ed5c0d031a2bf1253
janakhpon <jnovaxer@gmail.com>
2019-10-23 20:21:34 +0630

    GDEV029

---
+++ src/Components/DateDashboard/index.js
@@ -0,0 +1,153 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { LightgalleryProvider, LightgalleryItem } from "react-lightgallery";
+import { Link } from 'react-router-dom';
+import moment from 'moment';
+import 'lightgallery';
+import "animate.css/animate.min.css";
+import "lightgallery.js/dist/css/lightgallery.css";
+const TYPE_API_URL = 'http://localhost:5000/typeapi';
+const date_API_URL = 'http://localhost:5000/dateapi';
+const GALARIE_API_URL = 'http://localhost:5000/galarieapi';
+const indexnum = [];
+class Datedash extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            wallpapers: [],
+            dates: [],
+            types: [],
+            tempOptions:[],
+            indexNum:[],
+            date: '',
+            value: ''
+        };
+    }
+
+
+
+    getGalarie = (val) => {
+        let url = `${GALARIE_API_URL}/list`;
+        axios.get(url).then(response => response.data).then(data => {
+            data.forEach((da, i) =>{
+                if(moment(da.date).fromNow() === moment(val).fromNow()){
+                    indexnum.push(i);
+                }
+            })
+            this.setState({
+                wallpapers: data
+            })
+        });
+    }
+
+
+    getdate = () => {
+        
+    }
+
+
+    getType = () => {
+        let url = `${TYPE_API_URL}/list`;
+        axios.get(url).then(response => response.data).then(data => {
+            this.setState({
+                types: data
+            })
+        })
+    }
+
+
+
+    componentDidMount() {
+
+
+        if (this.props.location.state) {
+            const { date, tempOptions } = this.props.location.state
+            localStorage.setItem('date', date);
+            localStorage.setItem('tempOptions', tempOptions);
+
+        } else {
+            this.setState({
+                date: localStorage.getItem('date'),
+                tempOptions:localStorage.getItem('tempOptions')
+            })
+        }
+
+        console.log(localStorage.getItem('date'));
+
+        this.getGalarie(localStorage.getItem('date'));
+    }
+
+
+
+
+
+
+
+    render() {
+
+        return (
+            <div className="site-section" data-aos="fade">
+                <div className="container-fluid">
+
+                    <div className="row justify-content-center">
+
+                        <div className="col-md-7">
+                            <div className="row mb-5">
+                                <div className="col-12 ">
+                                    <div className="site-section-heading text-center">
+                                        <div className="input-group col-md-12">
+                                            <input type="text" className="form-control input-lg" placeholder="keywords ... " />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="row">
+                        <LightgalleryProvider>
+                            {indexnum.map((key) => {
+                                return (
+
+                                    <div id="aniimated-thumbnials" className="col-sm-6 col-md-4 col-lg-3 col-xl-2 item" data-aos="fade" key={key}>
+                                        <LightgalleryItem group="any" src={`http://localhost:5000/${this.state.wallpapers[key].image}`}
+                                            subHtml={`<h3>${this.state.wallpapers[key].title}</h3><p>${this.state.wallpapers[key].type}</p>`}>
+                                            <Link to={{
+                                                pathname: `/Wallpaper/${this.state.wallpapers[key]._id}`,
+                                                state: {
+                                                    title: this.state.wallpapers[key].title,
+                                                    description: this.state.wallpapers[key].description,
+                                                    choice: this.state.wallpapers[key].choice,
+                                                    rate: this.state.wallpapers[key].rate,
+                                                    image: this.state.wallpapers[key].image,
+                                                    devices: this.state.wallpapers[key].devices,
+                                                    keyword:this.state.wallpapers[key].keyword,
+                                                    type: this.state.wallpapers[key].type,
+                                                    date: this.state.wallpapers[key].date,
+                                                    mimetype: this.state.wallpapers[key].mimetype,
+                                                    id: this.state.wallpapers[key]._id
+                                                }
+                                            }}>
+                                                <img src={`http://localhost:5000/${this.state.wallpapers[key].image}`} alt={this.state.wallpapers[key].date} className="img-fluid" />
+                                            </Link>
+                                        </LightgalleryItem>
+                                    </div>
+
+                                );
+                            })}
+
+
+                        </LightgalleryProvider>
+
+
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default Datedash;
--- src/Components/Sorttag/index.js
+++ src/Components/Sorttag/index.js
@@ -1,7 +1,9 @@
 import React, { Fragment, Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import './index.css';
 const TYPE_API_URL = 'http://localhost:5000/typeapi';
 const TAG_API_URL = 'http://localhost:5000/tagapi';
 const GALARIE_API_URL = 'http://localhost:5000/galarieapi';
@@ -199,22 +201,27 @@ class Sorttag extends Component {
                             <div className="col-md-7">
                                 <div className="row mb-5">
                                     <div className="col-12 ">
-                                        <h2 className="site-section-heading text-center">Sort By Tag</h2>
+                                        <h2 className="site-section-heading text-center">Sort By Type</h2>
                                     </div>
                                 </div>
 
                                 <div className="row">
-                                    <div className="col-lg-8 mb-5">
-                                    <p className="mb-2 font-weight-bold text-center"> available tags </p>
+                                    <div className="col-lg-7 mb-5">
+                                        <p className="mb-2 font-weight-bold text-center"> available types </p>
                                         <div className="mb-4">
                                             {/**
                                                 starting from index 0 to 7
                                                 */}
                                             {
-                                                typeOptions.map((type, key) => {
+                                                tagOptions.map((tag, key) => {
                                                     let gid = Math.floor(Math.random() * 7) + 0;
                                                     return (
-                                                        <span className={`${typeclasses[gid]}`} key={key}>{type.value}</span>
+                                                        <Link to={{
+                                                            pathname: `/Dash-Tag`,
+                                                            state: {
+                                                              tag:tag.value
+                                                            }
+                                                          }}><span className={`${tagclasses[gid]}`} key={key}>{tag.value}</span></Link>
                                                     );
                                                 })
                                             }
@@ -222,15 +229,16 @@ class Sorttag extends Component {
 
                                         </div>
                                     </div>
-                                    <div className="col-lg-3 ml-auto">
-                                        <div className="input-group mb-4 border-0 rounded-pill p-1">
-                                            <div className="input-group-prepend border-0">
-                                                <button id="button-addon4" type="button" className="btn btn-link text-info"><FontAwesomeIcon icon={faSearch} /></button>
+                                    <div className="col-lg-4 ml-auto">
+                                        
+                                            <div className="input-group mb-4 border-0 rounded-pill p-1">
+                                                <div className="input-group-prepend border-0">
+                                                    <button id="button-addon4" type="button" className="btn btn-link text-info"><FontAwesomeIcon icon={faSearch} /></button>
+                                                </div>
+                                                <input type="search" placeholder="What're you searching for ? ... ." aria-describedby="button-addon4" className="form-control bg-none border-0" />
                                             </div>
-                                            <input type="search" placeholder="What're you searching for ? ... ." aria-describedby="button-addon4" className="form-control bg-none border-0" />
-                                        </div>
 
-
+                                        
 
                                     </div>
                                 </div>
